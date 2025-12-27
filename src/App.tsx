e7cdbfc2f60dd4bc0b29d636a13612af4e65d07e
Leonydis138 <138300852+Leonydis138@users.noreply.github.com>
2025-12-28 00:18:08 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import QuantumBIOS from "./components/QuantumBIOS"
+
+export default function App() {
+  return (
+    <div style={{ background: "#000", color: "#0ff", minHeight: "100vh" }}>
+      <QuantumBIOS />
+    </div>
+  )
+}
